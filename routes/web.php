abc886591f78ced2b1607f58908d57a74a05a996
nkanyi73 <nkanyi73@gmail.com>
2021-07-22 18:55:57 +0300

    Revert "Initial product controller commit"
    This reverts commit 5b96484435e4bd786a61444b5ac01b4659d87461.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::resource('products', ProductController::class);
-
+Route::get('/', function () {
+    return view('welcome');
+});
