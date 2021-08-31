b4961f404d70f84a66d0cc0181077703993dbaf6
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2021-08-31 09:13:05 +0300

    Add ui template

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,21 +5,7 @@
     <div class="container">
         <div class="flex flex-col md:flex-row py-20 md:py-24">
         <div
-             class="w-4/5 sm:w-1/2 md:w-2/5 lg:w-1/3 mx-auto lg:mx-0 flex md:flex-col lg:flex-row items-start md:items-center justify-start md:justify-center md:text-center lg:text-left md:border-r-2 last:border-r-0 md:border-primary-lighter pb-3 md:pb-0">
-            <div>
-                <img src="assets/img/icons/icon-shipping.svg"
-                     class="w-12 h-12 object-contain object-center"
-                     alt="icon"/>
-            </div>
-            <div class="ml-6 md:mt-3 lg:mt-0">
-                <h3 class="font-hk font-semibold text-primary text-xl tracking-wide">
-                    Free shipping</h3>
-                <p class="font-hk text-secondary-lighter text-base tracking-wide">
-                    On all orders over $30</p>
-            </div>
-        </div>
-        <div
-             class="w-4/5 sm:w-1/2 md:w-2/5 lg:w-1/3 mx-auto lg:mx-0 flex md:flex-col lg:flex-row items-start md:items-center justify-start md:justify-center md:text-center lg:text-left md:border-r-2 last:border-r-0 md:border-primary-lighter pb-3 md:pb-0">
+             class="w-4/5 sm:w-1/2 md:w-1/2 lg:w-1/2 mx-auto lg:mx-0 flex md:flex-col lg:flex-row items-start md:items-center justify-start md:justify-center md:text-center lg:text-left md:border-r-2 last:border-r-0 md:border-primary-lighter pb-3 md:pb-0">
             <div>
                 <img src="assets/img/icons/icon-support.svg"
                      class="w-12 h-12 object-contain object-center"
@@ -33,7 +19,7 @@
             </div>
         </div>
         <div
-             class="w-4/5 sm:w-1/2 md:w-2/5 lg:w-1/3 mx-auto lg:mx-0 flex md:flex-col lg:flex-row items-start md:items-center justify-start md:justify-center md:text-center lg:text-left md:border-r-2 last:border-r-0 md:border-primary-lighter pb-3 md:pb-0">
+             class="w-4/5 sm:w-1/2 md:w-1/2 lg:w-1/2 mx-auto lg:mx-0 flex md:flex-col lg:flex-row items-start md:items-center justify-start md:justify-center md:text-center lg:text-left md:border-r-2 last:border-r-0 md:border-primary-lighter pb-3 md:pb-0">
             <div>
                 <img src="assets/img/icons/icon-return.svg"
                      class="w-12 h-12 object-contain object-center"
